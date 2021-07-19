fdd0300a19e32065785881f418635eaaf59285dd
zktm9903 <zktm9903@gmail.com>
2021-07-19 11:09:05 +0900

    completed

---
+++ socket.js
@@ -0,0 +1,42 @@
+const { isObject } = require('util');
+
+var app = require('express')();
+var server = require('http').createServer(app);
+// http server를 socket.io server로 upgrade한다
+var io = require('socket.io')(server);
+
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index-room.html');
+});
+
+var chat = io.on('connection', function(socket) {
+  console.log('Conneted by something')
+  console.log(socket.id)
+  socket.on('chat', function(data){
+    console.log('message from client: ', data);
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit('chat', data);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+     //socket.emit('test', data);
+  });
+
+  // socket.on('chat message', function(data){
+  //   console.log('message from client: ', data);
+
+  //   var name = socket.name = data.name;
+  //   var room = socket.room = data.room;
+
+  //   // room에 join한다
+  //   socket.join(room);
+
+  //   // room에 join되어 있는 클라이언트에게 메시지를 전송한다
+  //   chat.to(room).emit('chat message', data.msg);
+  // });
+});
+
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
+});
